cebd88a9709b5f054f2eb600ae02b305340abfd2
unknown <adam.day7973@gmail.com>
2023-03-20 18:36:14 +1100

    updated js with var and pseudocode

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,7 @@
-var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast/daily?lat=44.34&lon=10.99&cnt=7&appid=65c9e335223e8ff274ce2918fe07a557'
+var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=Sydney&appid=65c9e335223e8ff274ce2918fe07a557'
+
+var weatherSearchEl = $('#weatherSearch');
+
 
 fetch(requestUrl)
   .then(function (response) {
@@ -8,3 +11,27 @@ fetch(requestUrl)
     console.log('Fetch Response \n-------------');
     console.log(data);
   });
+
+
+
+
+  // function for search form
+
+
+// function handleWeatherSearchSubmit(event) {
+//   event.preventDefault();
+// // creates variables for the search-input text space and format input dropdown
+//   var searchInputVal = document.querySelector('#search-input').value;
+//   var formatInputVal = document.querySelector('#format-input').value;
+// // creates if statement to check if search field is empty
+//   if (!searchInputVal) {
+//     console.error('You need a search input value!');
+//     return;
+//   }
+//   // creates a variable for the search functionality by placing the above input into the API search parameter
+//   var queryString = './search-results.html?q=' + searchInputVal + '&format=' + formatInputVal;
+//   // changes location to the variable above with the url of the searched input
+//   location.assign(queryString);
+// }
+// // event listenter for the submit button to run function
+// searchFormEl.addEventListener('submit', handleSearchFormSubmit);
