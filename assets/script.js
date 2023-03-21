002a5a95e3e585b09fb481251734bf02cda8cb00
unknown <adam.day7973@gmail.com>
2023-03-22 10:45:17 +1100

    updated style css, and finished wireframe for content, started on search functionality

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,5 @@
 var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=Sydney&appid=65c9e335223e8ff274ce2918fe07a557'
-
-var weatherSearchEl = $('#weatherSearch');
+var searchFormEl = $('#search-form');
 
 
 fetch(requestUrl)
@@ -14,24 +13,28 @@ fetch(requestUrl)
 
 
 
-
   // function for search form
+function handleWeatherSearchSubmit(event) {
+  event.preventDefault();
+// creates variables for the search-input text space and format input dropdown
+  var weatherSearchEl = $('#weather-Search').value;
+  console.log(weatherSearchEl);
+// creates if statement to check if search field is empty
+  if (!weatherSearchEl) {
+    console.error('You need a search input value!');
+    return;
+  }
+  // creates a variable for the search functionality by placing the above input into the API search parameter
+  var queryString = 'forecast?q=' + weatherSearchEl;
+}
+// event listenter for the submit button to run function
+searchFormEl.addEventListener('submit', handleWeatherSearchSubmit);
 
 
-// function handleWeatherSearchSubmit(event) {
-//   event.preventDefault();
-// // creates variables for the search-input text space and format input dropdown
-//   var searchInputVal = document.querySelector('#search-input').value;
-//   var formatInputVal = document.querySelector('#format-input').value;
-// // creates if statement to check if search field is empty
-//   if (!searchInputVal) {
-//     console.error('You need a search input value!');
-//     return;
-//   }
-//   // creates a variable for the search functionality by placing the above input into the API search parameter
-//   var queryString = './search-results.html?q=' + searchInputVal + '&format=' + formatInputVal;
-//   // changes location to the variable above with the url of the searched input
-//   location.assign(queryString);
-// }
-// // event listenter for the submit button to run function
-// searchFormEl.addEventListener('submit', handleSearchFormSubmit);
+// take search parameters for city and replace api fetch url - can I splice the original fetch from line 1 or should i create own fetch inside function?
+
+
+// save search into local storage
+
+
+// create memory button for each saved local storage param
