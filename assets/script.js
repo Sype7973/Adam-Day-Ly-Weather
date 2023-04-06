282407f2c119ab3748ae65048b6679f87fa28a73
unknown <adam.day7973@gmail.com>
2023-04-06 20:38:32 +1000

    updated fetch functionality to check undefined parameters or random input, updated button creation on local storage if same input, debugged and added css styling

--- assets/script.js
+++ assets/script.js
@@ -44,69 +44,67 @@ function getWeather(query){
      
         
       // Extract necessary data from API response
-      let today = data.list[0];
-      let forecast = data.list.filter(function(item, index) {
-        return (index + 1) % 8 === 0 // Keep only one forecast per day (every 8th item)
-      });
-
-      // Create elements for current day weather
-      let currentTitle = $('<h2 class = "title" id = weather-header>');
-      currentTitle.text("Today's Current Weather in " + query);
-      cityTitle.append(currentTitle);
-      var titleWeatherCard = $('<div class = "content" id = main-forecast-content>');
-      let currentIcon = $('<img>').attr('src', 'http://openweathermap.org/img/wn/' + today.weather[0].icon + '.png').addClass('weather-icon');
-      titleWeatherCard.append(currentIcon);
-      let currentTemp = $('<div>').text(Math.round(today.main.temp - 273.15) + '°C').addClass('weather-temp');
-      titleWeatherCard.append(currentTemp);
-      let currentDesc = $('<div>').text(today.weather[0].description).addClass('weather-desc');
-      titleWeatherCard.append(currentDesc);
-      let currentWind = $('<div>').text("Wind Speed: " + today.wind.speed + " m/s").addClass('weather-wind');
-      titleWeatherCard.append(currentWind);
-      cityTitle.append(titleWeatherCard);
+      if (data.cod === '404') {
+        alert('City not found on OpenWeather API');
+      } else {
+        // Add city to array of searched cities in local storage
+        searchedCityHistory.push(query);
+        localStorage.setItem('City', JSON.stringify(searchedCityHistory));
       
+        // Create elements for current day weather
+        let today = data.list[0];
+        let forecast = data.list.filter(function(item, index) {
+          return (index + 1) % 8 === 0 // Keep only one forecast per day (every 8th item)
+        });
       
-      // Create elements for next 5 days weather
-      forecast.forEach(function(item) {
-        var weatherCard = $('<div class = "content" id = weather-forecast-content>');
-        weatherforecastResults.append(weatherCard);
-        let icon = $('<img>').attr('src', 'http://openweathermap.org/img/wn/' + item.weather[0].icon + '.png').addClass('weather-icon');
-        weatherCard.append(icon);
-        let date = $('<div>').text(dayjs(item.dt_txt).format('dddd')).addClass('weather-date');
-        weatherCard.append(date);
-        let temp = $('<div>').text(Math.round(item.main.temp - 273.15) + '°C').addClass('weather-temp');
-        weatherCard.append(temp);
-        let desc = $('<div>').text(item.weather[0].description).addClass('weather-desc');
-        weatherCard.append(desc);
-        let windSpeed = $('<div>').text("Wind Speed: " + item.wind.speed + " m/s").addClass('weather-wind');
-        weatherCard.append(windSpeed);
-      });
-      searchedCityHistory.push(query);
-      localStorage.setItem("City", JSON.stringify(searchedCityHistory));
-      loadSearchHistory(query);
-    },
-    error: function(jqxHR, textStatus, errorthrown) {
-      // Alert if city doesn't exist on OpenWeather API
-      if (jqxHR.status === 404) {
-        alert('Sorry that isn\'t a valid city name!');
-        searchedCityHistory.pop();
-        localStorage.setItem("City", JSON.stringify(searchedCityHistory));
-      } 
+        let currentTitle = $('<h2 class="title" id="weather-header">');
+        currentTitle.text("Today's Current Weather in " + query);
+        cityTitle.append(currentTitle);
+        let titleWeatherCard = $('<div class="content" id="main-forecast-content">');
+        let currentIcon = $('<img>').attr('src', 'http://openweathermap.org/img/wn/' + today.weather[0].icon + '.png').addClass('weather-icon');
+        titleWeatherCard.append(currentIcon);
+        let currentTemp = $('<div>').text(Math.round(today.main.temp - 273.15) + '°C').addClass('weather-temp');
+        titleWeatherCard.append(currentTemp);
+        let currentDesc = $('<div>').text(today.weather[0].description).addClass('weather-desc');
+        titleWeatherCard.append(currentDesc);
+        let currentWind = $('<div>').text("Wind Speed: " + today.wind.speed + " m/s").addClass('weather-wind');
+        titleWeatherCard.append(currentWind);
+        cityTitle.append(titleWeatherCard);
+      
+        forecast.forEach(function(item) {
+          let weatherCard = $('<div class="content" id="weather-forecast-content">');
+          weatherforecastResults.append(weatherCard);
+          let icon = $('<img>').attr('src', 'http://openweathermap.org/img/wn/' + item.weather[0].icon + '.png').addClass('weather-icon');
+          weatherCard.append(icon);
+          let date = $('<div>').text(dayjs(item.dt_txt).format('dddd')).addClass('weather-date');
+          weatherCard.append(date);
+          let temp = $('<div>').text(Math.round(item.main.temp - 273.15) + '°C').addClass('weather-temp');
+          weatherCard.append(temp);
+          let desc = $('<div>').text(item.weather[0].description).addClass('weather-desc');
+          weatherCard.append(desc);
+          let windSpeed = $('<div>').text("Wind Speed: " + item.wind.speed + " m/s").addClass('weather-wind');
+          weatherCard.append(windSpeed);
+        });
+      
+        loadSearchHistory();
+      }
     }
-});
+  });
 }
 // function to load search history
-function loadSearchHistory(){
+function loadSearchHistory() {
   let searchedCityHistory = JSON.parse(localStorage.getItem('City')) || [];
-    // Clear existing buttons
-    $('#btn-memory').empty();
-    // Render a button for each city in local storage
-    searchedCityHistory.forEach(function(city) {
-      let button = $('<button>').addClass('city-button button column is-primary is-large buttons are-medium').text(city);
-      $('#btn-memory').append(button);
-    
-    });
-  }
-
+  // Clear existing buttons
+  $('#btn-memory').empty();
+  // Render a button for each city in local storage and remove duplicates if any
+  // 'set' operator removes duplicates by converting array to set and back to array
+  // it will only keep unique values
+  let uniqueCities = [...new Set(searchedCityHistory)]; 
+  uniqueCities.forEach(function(city) {
+    let button = $('<button>').addClass('city-button button column is-primary is-large are-medium is-responsive').text(city);
+    $('#btn-memory').append(button);
+  });
+}
 // Attach click event to each button to re-open its previous search
 $('#btn-memory').on('click', function(event) {
   event.stopPropagation();
